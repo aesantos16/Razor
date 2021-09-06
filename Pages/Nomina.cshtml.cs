4a0dfaf73ce96f4707338d90ad621ce1079e9b32
Carlos Vilorio <cvilorio@VIMENCA.local>
2021-09-06 10:20:51 -0400

    CV: Refactoring Nomina

--- Pages/Nomina.cshtml.cs
+++ Pages/Nomina.cshtml.cs
@@ -6,37 +6,70 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Razor.Practica2.Pages
-{   
+{
     public class NominaModel : PageModel
-    {  
-        
-        public string Nombres_empleado1 { get; set; } = "Alejandro Emmanuel";
-        public string Apellidos_empleado1 { get; set; } = "Santos De los santos";
-        public string cargo_empleado1 { get; set; } = "Soporte Junior";
-        public string  salario_mensual_empleado1 { get; set; } = "40,000.00";
-        public string Nombres_empleado2 { get; set; } = "Carlos Manuel";
-        public string Apellidos_empleado2 { get; set; } = "Tejada ";
-        public string cargo_empleado2 { get; set; } = "Soporte Senior";
-        public string salario_mensual_empleado2 { get; set; } = "45,000.00";
-        public string Nombres_empleado3 { get; set; } = "Carlos";
-        public string Apellidos_empleado3 { get; set; } = "Rodriguez Then";
-        public string cargo_empleado3 { get; set; } = "Soporte Senior";
-        public string salario_mensual_empleado3 { get; set; } = "60,000.00";
-        public string Nombres_empleado4 { get; set; } = "Victor Manuel";
-        public string Apellidos_empleado4 { get; set; } = "Mendez Saba";
-        public string cargo_empleado4 { get; set; } = "Soporte senior";
-        public string salario_mensual_empleado4 { get; set; } = "65,000.00";
-        public string Nombres_empleado5 { get; set; } = "Aguntin";
-        public string Apellidos_empleado5 { get; set; } = "Reynoso Lara";
-        public string cargo_empleado5 { get; set; } = "Soporte Junior";
-        public string salario_mensual_empleado5 { get; set; } = "35,000.00";
-        public string Nombres_empleado6 { get; set; } = "Michael";
-        public string Apellidos_empleado6 { get; set; } = "Bernald";
-        public string cargo_empleado6 { get; set; } = "Soporte Junior";
-        public string salario_mensual_empleado6 { get; set; } = "85,000.00";
+    {
+        public List<EmpleadoModel> Empleados { get; set; }
 
         public void OnGet()
         {
+            Empleados = new List<EmpleadoModel>();
+
+            Empleados.Add(new EmpleadoModel
+            {
+                Nombres_empleado = "Alejandro Emmanuel",
+                Apellidos_empleado = "Santos De los santos",
+                Cargo_empleado = "Soporte Junior",
+                Salario_mensual_empleado = "40,000.00",
+            });
+
+            Empleados.Add(new EmpleadoModel
+            {
+                Nombres_empleado = "Carlos Manuel",
+                Apellidos_empleado = "Tejada ",
+                Cargo_empleado = "Soporte Senior",
+                Salario_mensual_empleado = "45,000.00",
+            });
+
+            Empleados.Add(new EmpleadoModel
+            {
+                Nombres_empleado = "Carlos",
+                Apellidos_empleado = "Rodriguez Then",
+                Cargo_empleado = "Soporte Senior",
+                Salario_mensual_empleado = "60,000.00",
+            });
+
+            Empleados.Add(new EmpleadoModel
+            {
+                Nombres_empleado = "Victor Manuel",
+                Apellidos_empleado = "Mendez Saba",
+                Cargo_empleado = "Soporte senior",
+                Salario_mensual_empleado = "65,000.00",
+            });
+
+            Empleados.Add(new EmpleadoModel
+            {
+                Nombres_empleado = "Aguntin",
+                Apellidos_empleado = "Reynoso Lara",
+                Cargo_empleado = "Soporte Junior",
+                Salario_mensual_empleado = "35,000.00",
+            });
+
+            Empleados.Add(new EmpleadoModel
+            {
+                Nombres_empleado = "Michael",
+                Apellidos_empleado = "Bernald",
+                Cargo_empleado = "Soporte Junior",
+                Salario_mensual_empleado = "85,000.00",
+            });
         }
+    }
+
+    public class EmpleadoModel
+    {
+        public string Nombres_empleado { get; set; }
+        public string Apellidos_empleado { get; set; }
+        public string Cargo_empleado { get; set; }
+        public string Salario_mensual_empleado { get; set; }
     }
 }
